fb98f187b06e2a5aad7af60a5794d4637673f1ae
Nate Beach <nate.beach@gmail.com>
2018-05-17 19:05:59 -0700

    updated function name, added dice 'description', e.g. 2d6 added sample query

--- diceroll.go
+++ diceroll.go
@@ -7,37 +7,35 @@ import (
 )
 
 
-func RollDie(num int, max int, player string) int {
+func rollDie(numDie int, sides int, player string) int {
 	rand.Seed(time.Now().UTC().UnixNano())
 	i := 0
 	result := 0
-	for i < num {
-		result = result + (1 + rand.Intn(max))
+	for i < numDie {
+		result = result + (1 + rand.Intn(sides))
 		i++
 	}
-    fmt.Printf("%s rolls a %dd%d for %d\n", player, num, max, result)
+    fmt.Printf("%s rolls a %dd%d for %d\n", player, numDie, sides, result)
 
-    dice := fmt.Sprintf("%dd%d", num, max)
-    q := fmt.Sprintf("INSERT INTO user_tbl (name, dice, result) VALUES (%s, %s, %d)", player, dice, result)
+    dice := fmt.Sprintf("%dd%d", numDie, sides)
+    q := fmt.Sprintf("INSERT INTO user_tbl (name, dice, result) VALUES (%s, %s, %d);", player, dice, result)
     fmt.Println(dice)
     fmt.Println(q)
 	return result // or whatever, query, 2d6... 
-
 }
 
 
-
 func main() {
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
-    RollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
+    rollDie(2, 12, "nate")
 }
 
