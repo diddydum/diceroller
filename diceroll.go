1f942a313ae56659b55fcff3ec8c418ccb7a5cce
diddydum <37198933+diddydum@users.noreply.github.com>
2018-05-20 09:40:18 -0700

    run goimports/gofmt

--- diceroll.go
+++ diceroll.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
-
 
 func rollDie(numDie int, sides int, player string) int {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -15,27 +14,25 @@ func rollDie(numDie int, sides int, player string) int {
 		result = result + (1 + rand.Intn(sides))
 		i++
 	}
-    fmt.Printf("%s rolls a %dd%d for %d\n", player, numDie, sides, result)
+	fmt.Printf("%s rolls a %dd%d for %d\n", player, numDie, sides, result)
 
-    dice := fmt.Sprintf("%dd%d", numDie, sides)
-    q := fmt.Sprintf("INSERT INTO user_tbl (name, dice, result) VALUES (%s, %s, %d);", player, dice, result)
-    fmt.Println(dice)
-    fmt.Println(q)
-	return result // or whatever, query, 2d6... 
+	dice := fmt.Sprintf("%dd%d", numDie, sides)
+	q := fmt.Sprintf("INSERT INTO user_tbl (name, dice, result) VALUES (%s, %s, %d);", player, dice, result)
+	fmt.Println(dice)
+	fmt.Println(q)
+	return result // or whatever, query, 2d6...
 }
-
 
 func main() {
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
-    rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
+	rollDie(2, 12, "nate")
 }
-
